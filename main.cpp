3fa53ae66d605b780493865c635ff426307c3583
DarthBob112112 <abhijithrajunair@gmail.com>
2025-10-13 00:53:22 +0530

    emisive cameramatrix pass

--- main.cpp
+++ main.cpp
@@ -82,6 +82,11 @@ int main() {
 
 		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		emissiveShader.Activate();
+		emissiveShader.Setmat4("cameraMatrix", scenecam.GetTransformMatrix());
+		//emissiveShader.Setmat4("model", model);
+
 		diffuseShader.Activate();
 		diffuseShader.Setmat4("cameraMatrix", scenecam.GetTransformMatrix());
 		diffuseShader.Setvec3("cameraPos", scenecam.Position);
