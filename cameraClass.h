2791ed77e77f5c57296e9ad4770ea9a0302850f5
sanketbanket <www.sanketramteke13@gmail.com>
2025-10-12 11:01:13 +0530

    Add Window and GLM

---
+++ cameraClass.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef CAMERA_CLASS_H
+#define CAMERA_CLASS_H
+
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/glm.hpp"
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "glm/gtx/vector_angle.hpp"
+
+class Camera {
+public:
+
+	glm::vec3 Position = glm::vec3(0.0f, 0.0f, 0.0f);   // position vector 
+	glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, 0.0f); // direction vector
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection = glm::mat4(1.0f);
+
+	glm::vec3 local_right, local_up;
+	glm::vec3 fake_up, fake_right;
+	double xposMouse, yposMouse;
+
+	Camera(glm::vec3 position, glm::vec3 target, float fov, float aRatio, float nearest, float furthest);
+	glm::mat4 GetTransformMatrix();
+	void GetKeyInputs(GLFWwindow* window, float velocity, bool debug);
+	void ghetto_Mouse_Callback(GLFWwindow* window);
+
+
+	bool take_first_mouse_location = true;
+
+};
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -2,8 +2,41 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+using std::cout, std::endl;
+
 int main() {
-	std::cout << "Hello, OpenGL!" << std::endl;
+
+	// Basic GLFW window creation
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_SAMPLES, 4); // Anti-aliasing
+	//glEnable(GL_MULTISAMPLE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Renderer", NULL, NULL);
+
+	if (window == NULL) {
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, 800, 600);
+	glEnable(GL_DEPTH_TEST);
+
+
+	// Clearing background color and rendering loop
+
+
+	while (glfwWindowShouldClose(window) == false) {
+		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
 	return 0;
 	
 }
